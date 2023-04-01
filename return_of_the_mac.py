4cb8b50b9eae0fc76a69b00624af32e22dc4dd01
Stefan S <32467276+GloriousEstefan@users.noreply.github.com>
2023-04-01 14:08:13 -0400

    Delete return_of_the_mac.py

--- return_of_the_mac.py
+++
@@ -1,9 +0,0 @@
-from is_ip_valid import is_ip_valid
-from ios_ssh import ios_ssh
-from napalm_get_mac import napalm_get_mac
-import sys
-
-device_ip = is_ip_valid()
-ssh_connection = ios_ssh(device_ip)
-mac_dict = napalm_get_mac(ssh_connection)
-print(mac_dict)
